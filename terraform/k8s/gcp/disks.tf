87e507abcf5afcf2701e60a8c32e3fda87e71ec7
Tomasz Knopik <tknopik@nvidia.com>
2020-11-04 11:09:28 +0100

    terraform: use xfs with gcp disks

--- terraform/k8s/gcp/disks.tf
+++ terraform/k8s/gcp/disks.tf
@@ -15,38 +15,11 @@ resource "kubernetes_storage_class" "ais-storage-class" {
 
   parameters = {
     type = "pd-standard"
-    # fstype = "xfs" # TODO: make it work
+    fstype = "xfs"
   }
 
   volume_binding_mode = "WaitForFirstConsumer"
   allow_volume_expansion = true
 
-
-  #TODO: revisit these options
-  #mount_options = ["file_mode=0700", "dir_mode=0777", "mfsymlinks", "uid=1000", "gid=1000", "nobrl", "cache=none"]
+  mount_options = ["noatime","nodiratime","logbufs=8","logbsize=256k","largeio","inode64","swalloc","allocsize=8192k"]
 }
-
-# TODO: not needed for now as we use volumeClaimTemplates directly in StatefulSet spec.
-# which never really happens as it is dynamic PVC
-//resource "kubernetes_persistent_volume_claim" "ais-storage-claim-30" {
-//  metadata {
-//    name = "ais-claim"
-//  }
-//
-//  spec {
-//    access_modes = ["ReadWriteOnce"]
-//    storage_class_name = "ais"
-//
-//    resources {
-//      requests = {
-//        storage = "30Gi"
-//      }
-//    }
-//  }
-//
-//  depends_on = [
-//    kubernetes_storage_class.ais-storage-class
-//  ]
-//}
-
-
