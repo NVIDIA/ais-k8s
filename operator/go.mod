ff7b2ab895af94adfa44f2dd35fffcf0138af137
Janusz Marcinkiewicz <januszm@nvidia.com>
2021-04-16 12:43:55 +0200

    operator: upgrade Go1.15 to Go1.16
    Signed-off-by: Janusz Marcinkiewicz <januszm@nvidia.com>

--- operator/go.mod
+++ operator/go.mod
@@ -1,6 +1,6 @@
 module github.com/ais-operator
 
-go 1.15
+go 1.16
 
 require (
 	github.com/NVIDIA/aistore v0.0.0-20210413234838-8e029edd880b
--- terraform/scripts/ci-test-script.sh
+++ terraform/scripts/ci-test-script.sh
@@ -3,9 +3,9 @@
 apt-get -y update
 apt-get install -y build-essential
 
-curl -LO https://storage.googleapis.com/golang/go1.15.linux-amd64.tar.gz
-tar -C /usr/local -xvzf go1.15.linux-amd64.tar.gz > /dev/null 2>&1
-rm -rf go1.15.linux-amd64.tar.gz
+curl -LO https://storage.googleapis.com/golang/go1.16.linux-amd64.tar.gz
+tar -C /usr/local -xvzf go1.16.linux-amd64.tar.gz > /dev/null 2>&1
+rm -rf go1.16.linux-amd64.tar.gz
 
 git clone https://github.com/NVIDIA/aistore.git
 cd aistore/cmd/cli/test && /usr/local/go/bin/go test -v .
