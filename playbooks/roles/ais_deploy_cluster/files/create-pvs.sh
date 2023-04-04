70fd39ee21a8bcf7b0d29ab8fe0f580334daf469
Prashanth Dintyala <vdintyala@nvidia.com>
2023-04-04 14:00:11 -0700

    update local pv creation scripts
    Signed-off-by: Prashanth Dintyala <vdintyala@nvidia.com>

--- playbooks/roles/ais_deploy_cluster/files/create-pvs.sh
+++ playbooks/roles/ais_deploy_cluster/files/create-pvs.sh
@@ -6,6 +6,7 @@
 #
 
 mpaths=${MPATHS:-"/ais/sda /ais/sdb /ais/sdc /ais/sdd /ais/sde /ais/sdf /ais/sdg /ais/sdh /ais/sdi /ais/sdj"}
+namespace=${NAMESPACE:-"ais"}
 mpath_size=${MPATH_SIZE:-"9Ti"}
 
 source_dir=$(dirname "${BASH_SOURCE[0]}")
@@ -16,6 +17,7 @@ if [[ -z ${nodes} ]]; then
     nodes=$(kubectl get nodes | tail -n +2 | awk '{print $1}')
 fi
 
+target_num=0
 for n in ${nodes} ; do
     for m in ${mpaths}; do
         name="$n-pv${m//\//\-}"
@@ -24,8 +26,11 @@ for n in ${nodes} ; do
         export NODE=$n
         export MPATH_LABEL=pv${m//\//\-}
         export MPATH_SIZE=$mpath_size
+        export NAMESPACE=$namespace
+        export CLAIM_NAME=ais${m//\//\-}-ais-target-$target_num
         envsubst < "${source_dir}"/pv.template.yaml > /tmp/pv.yaml
         kubectl apply -f /tmp/pv.yaml
         rm /tmp/pv.yaml
     done
+    target_num=$((target_num+1))
 done
