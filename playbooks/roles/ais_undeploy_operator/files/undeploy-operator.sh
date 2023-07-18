eff3bec4e6ff7526caa2e0dcb3266ed04b789113
Aaron Wilson <aawilson@nvidia.com>
2023-07-18 12:22:28 -0500

    Update default images and stats exporting
    - Update versions for ais operator and aisnode
    - Update docker image for ais-init
    - Default to use statsD if AIS_PROMETHEUS variable not set
    
    Signed-off-by: Aaron Wilson <aawilson@nvidia.com>

--- playbooks/roles/ais_undeploy_operator/files/undeploy-operator.sh
+++ playbooks/roles/ais_undeploy_operator/files/undeploy-operator.sh
@@ -4,6 +4,6 @@
 # Deploy AIS operator
 #
 
-release_version=${RELEASE:-v0.7}
+release_version=${RELEASE:-v0.94}
 
 kubectl delete -f https://github.com/NVIDIA/ais-k8s/releases/download/${release_version}/ais-operator.yaml
