697bd1b9d37f0b3d48e4077f6b689df82add569d
Abhishek Gaikwad <gaikwadabhishek1997@gmail.com>
2024-04-22 21:17:45 -0700

    operator: bump default version to v1.1.0
    Signed-off-by: Abhishek Gaikwad <gaikwadabhishek1997@gmail.com>

--- playbooks/ais-deployment/roles/ais_undeploy_operator/files/undeploy-operator.sh
+++ playbooks/ais-deployment/roles/ais_undeploy_operator/files/undeploy-operator.sh
@@ -4,6 +4,6 @@
 # Deploy AIS operator
 #
 
-release_version=${RELEASE:-v1.0.0}
+release_version=${RELEASE:-v1.1.0}
 
 kubectl delete -f https://github.com/NVIDIA/ais-k8s/releases/download/${release_version}/ais-operator.yaml
