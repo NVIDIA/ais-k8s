a8541463c4c5983d9abc59059faaa25d594116ca
Aaron Wilson <aawilson@nvidia.com>
2024-06-25 11:20:28 -0500

    operator: Refactor cleanup functions and wait for cleanup job success before finishing
    Signed-off-by: Aaron Wilson <aawilson@nvidia.com>

---
+++ operator/pkg/controllers/cluster_cleanup.go
@@ -0,0 +1,151 @@
+// Package controllers contains k8s controller logic for AIS cluster
+/*
+* Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
+ */
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	aisv1 "github.com/ais-operator/api/v1beta1"
+	"github.com/ais-operator/pkg/resources/cmn"
+	"github.com/ais-operator/pkg/resources/proxy"
+	"github.com/ais-operator/pkg/resources/statsd"
+	"github.com/ais-operator/pkg/resources/target"
+	batchv1 "k8s.io/api/batch/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func (r *AIStoreReconciler) cleanup(ctx context.Context, ais *aisv1.AIStore) (updated bool, err error) {
+	var nodeNames map[string]bool
+	nodeNames, err = r.client.ListNodesRunningAIS(ctx, ais)
+	if err != nil {
+		r.log.Error(err, "Failed to list nodes running AIS")
+	}
+
+	updated, err = cmn.AnyFunc(
+		func() (bool, error) { return r.cleanupTarget(ctx, ais) },
+		func() (bool, error) { return r.cleanupProxy(ctx, ais) },
+		func() (bool, error) { return r.client.DeleteConfigMapIfExists(ctx, statsd.ConfigMapNSName(ais)) },
+		func() (bool, error) { return r.cleanupRBAC(ctx, ais) },
+		func() (bool, error) { return r.cleanupPVC(ctx, ais) },
+	)
+	if updated {
+		jobs, err := r.createCleanupJobs(ctx, ais, nodeNames)
+		if err != nil {
+			r.log.Error(err, "Failed to run manual cleanup job")
+		}
+		timeoutCtx, cancel := context.WithTimeout(ctx, 1*time.Minute)
+		defer cancel()
+		if err := r.waitForJobs(timeoutCtx, jobs); err != nil {
+			r.log.Error(err, "Error while waiting for cleanup jobs to complete")
+		}
+	}
+	return updated, err
+}
+
+func (r *AIStoreReconciler) createCleanupJobs(ctx context.Context, ais *aisv1.AIStore, uniqueNodeNames map[string]bool) ([]*batchv1.Job, error) {
+	if ais.Spec.StateStorageClass != nil {
+		return nil, nil
+	}
+
+	r.log.Info("Running manual cleanup job")
+	jobs := make([]*batchv1.Job, 0, len(uniqueNodeNames))
+
+	for nodeName := range uniqueNodeNames {
+		r.log.Info("Creating cleanup job for node", "node", nodeName)
+		jobDef := cmn.NewCleanupJob(ais, nodeName)
+		if err := r.client.Create(ctx, jobDef); err != nil {
+			return jobs, err
+		}
+		jobs = append(jobs, jobDef)
+	}
+	return jobs, nil
+}
+
+func (r *AIStoreReconciler) waitForJobs(ctx context.Context, jobs []*batchv1.Job) error {
+	for _, job := range jobs {
+		r.log.Info("Waiting for job to complete", "jobName", job.Name)
+		for {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			default:
+				jobStatus := &batchv1.Job{}
+				if err := r.client.Get(ctx, types.NamespacedName{Name: job.Name, Namespace: job.Namespace}, jobStatus); err != nil {
+					r.log.Error(err, "Failed to get job status", "jobName", job.Name)
+					return err
+				}
+				if jobStatus.Status.Succeeded > 0 {
+					r.log.Info("Job completed successfully", "jobName", job.Name)
+					break
+				}
+				if jobStatus.Status.Failed > 0 {
+					err := fmt.Errorf("job %s failed", job.Name)
+					r.log.Error(err, "Job failed", "jobName", job.Name)
+					return err
+				}
+				time.Sleep(2 * time.Second)
+			}
+		}
+	}
+	return nil
+}
+
+func (r *AIStoreReconciler) cleanupPVC(ctx context.Context, ais *aisv1.AIStore) (bool, error) {
+	if ais.Spec.CleanupData != nil && *ais.Spec.CleanupData {
+		return r.deleteAllPVCs(ctx, ais)
+	}
+	if ais.Spec.StateStorageClass != nil {
+		return r.deleteStatePVCs(ctx, ais)
+	}
+	return false, nil
+}
+
+func (r *AIStoreReconciler) deleteAllPVCs(ctx context.Context, ais *aisv1.AIStore) (bool, error) {
+	r.log.Info("Cleaning up all target PVCs")
+	updated, err := r.client.DeletePVCs(ctx, ais.Namespace, target.PodLabels(ais), nil)
+	if err != nil {
+		return updated, err
+	}
+	r.log.Info("Cleaning up all proxy PVCs")
+	return r.client.DeletePVCs(ctx, ais.Namespace, proxy.PodLabels(ais), nil)
+}
+
+// Cleans up only dynamically created volumes by adding a filter by the defined state storage class
+func (r *AIStoreReconciler) deleteStatePVCs(ctx context.Context, ais *aisv1.AIStore) (bool, error) {
+	r.log.Info("Cleaning up dynamic target PVCs")
+	updated, err := r.client.DeletePVCs(ctx, ais.Namespace, target.PodLabels(ais), ais.Spec.StateStorageClass)
+	if err != nil {
+		return updated, err
+	}
+	r.log.Info("Cleaning up dynamic proxy PVCs")
+	return r.client.DeletePVCs(ctx, ais.Namespace, proxy.PodLabels(ais), ais.Spec.StateStorageClass)
+}
+
+func (r *AIStoreReconciler) cleanupRBAC(ctx context.Context, ais *aisv1.AIStore) (anyUpdated bool, err error) {
+	return cmn.AnyFunc(
+		func() (bool, error) {
+			crb := cmn.NewAISRBACClusterRoleBinding(ais)
+			return r.client.DeleteResourceIfExists(ctx, crb)
+		},
+		func() (bool, error) {
+			cluRole := cmn.NewAISRBACClusterRole(ais)
+			return r.client.DeleteResourceIfExists(ctx, cluRole)
+		},
+		func() (bool, error) {
+			rb := cmn.NewAISRBACRoleBinding(ais)
+			return r.client.DeleteResourceIfExists(ctx, rb)
+		},
+		func() (bool, error) {
+			role := cmn.NewAISRBACRole(ais)
+			return r.client.DeleteResourceIfExists(ctx, role)
+		},
+		func() (bool, error) {
+			sa := cmn.NewAISServiceAccount(ais)
+			return r.client.DeleteResourceIfExists(ctx, sa)
+		},
+	)
+}
--- operator/pkg/controllers/cluster_controller.go
+++ operator/pkg/controllers/cluster_controller.go
@@ -21,7 +21,6 @@ import (
 	"github.com/ais-operator/pkg/resources/cmn"
 	"github.com/ais-operator/pkg/resources/proxy"
 	"github.com/ais-operator/pkg/resources/statsd"
-	"github.com/ais-operator/pkg/resources/target"
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
@@ -171,44 +170,6 @@ func (r *AIStoreReconciler) handleCRDeletion(ctx context.Context, ais *aisv1.AIS
 	return reconcile.Result{}, nil
 }
 
-func (r *AIStoreReconciler) cleanup(ctx context.Context, ais *aisv1.AIStore) (updated bool, err error) {
-	var nodeNames map[string]bool
-	nodeNames, err = r.client.ListNodesRunningAIS(ctx, ais)
-	if err != nil {
-		r.log.Error(err, "Failed to list nodes running AIS")
-	}
-
-	updated, err = cmn.AnyFunc(
-		func() (bool, error) { return r.cleanupTarget(ctx, ais) },
-		func() (bool, error) { return r.cleanupProxy(ctx, ais) },
-		func() (bool, error) { return r.client.DeleteConfigMapIfExists(ctx, statsd.ConfigMapNSName(ais)) },
-		func() (bool, error) { return r.cleanupRBAC(ctx, ais) },
-		func() (bool, error) { return r.cleanupPVC(ctx, ais) },
-	)
-	if updated {
-		err := r.runManualCleanupJob(ctx, ais, nodeNames)
-		if err != nil {
-			r.log.Error(err, "Failed to run manual cleanup job")
-		}
-		// TODO: wait on job status to succeed before we remove the finalizer from the CR
-	}
-	return updated, err
-}
-
-func (r *AIStoreReconciler) runManualCleanupJob(ctx context.Context, ais *aisv1.AIStore, uniqueNodeNames map[string]bool) error {
-	if ais.Spec.StateStorageClass != nil {
-		return nil
-	}
-	r.log.Info("Running manual cleanup job")
-	for nodeName := range uniqueNodeNames {
-		r.log.Info("Creating cleanup job for node", "node", nodeName)
-		if err := r.client.Create(ctx, cmn.NewCleanupJob(ais, nodeName)); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func (r *AIStoreReconciler) attemptGracefulDecommission(params *aisapi.BaseParams, cleanupData bool) {
 	r.log.Info("Attempting graceful decommission of cluster")
 	if err := aisapi.DecommissionCluster(*params, cleanupData); err != nil {
@@ -221,62 +182,6 @@ func (r *AIStoreReconciler) attemptGracefulShutdown(params *aisapi.BaseParams) {
 	if err := aisapi.ShutdownCluster(*params); err != nil {
 		r.log.Error(err, "Failed to gracefully shutdown cluster")
 	}
-}
-
-func (r *AIStoreReconciler) cleanupPVC(ctx context.Context, ais *aisv1.AIStore) (bool, error) {
-	if ais.Spec.CleanupData != nil && *ais.Spec.CleanupData {
-		return r.deleteAllPVCs(ctx, ais)
-	}
-	if ais.Spec.StateStorageClass != nil {
-		return r.deleteStatePVCs(ctx, ais)
-	}
-	return false, nil
-}
-
-func (r *AIStoreReconciler) deleteAllPVCs(ctx context.Context, ais *aisv1.AIStore) (bool, error) {
-	r.log.Info("Cleaning up all target PVCs")
-	updated, err := r.client.DeletePVCs(ctx, ais.Namespace, target.PodLabels(ais), nil)
-	if err != nil {
-		return updated, err
-	}
-	r.log.Info("Cleaning up all proxy PVCs")
-	return r.client.DeletePVCs(ctx, ais.Namespace, proxy.PodLabels(ais), nil)
-}
-
-// Cleans up only dynamically created volumes by adding a filter by the defined state storage class
-func (r *AIStoreReconciler) deleteStatePVCs(ctx context.Context, ais *aisv1.AIStore) (bool, error) {
-	r.log.Info("Cleaning up dynamic target PVCs")
-	updated, err := r.client.DeletePVCs(ctx, ais.Namespace, target.PodLabels(ais), ais.Spec.StateStorageClass)
-	if err != nil {
-		return updated, err
-	}
-	r.log.Info("Cleaning up dynamic proxy PVCs")
-	return r.client.DeletePVCs(ctx, ais.Namespace, proxy.PodLabels(ais), ais.Spec.StateStorageClass)
-}
-
-func (r *AIStoreReconciler) cleanupRBAC(ctx context.Context, ais *aisv1.AIStore) (anyUpdated bool, err error) {
-	return cmn.AnyFunc(
-		func() (bool, error) {
-			crb := cmn.NewAISRBACClusterRoleBinding(ais)
-			return r.client.DeleteResourceIfExists(ctx, crb)
-		},
-		func() (bool, error) {
-			cluRole := cmn.NewAISRBACClusterRole(ais)
-			return r.client.DeleteResourceIfExists(ctx, cluRole)
-		},
-		func() (bool, error) {
-			rb := cmn.NewAISRBACRoleBinding(ais)
-			return r.client.DeleteResourceIfExists(ctx, rb)
-		},
-		func() (bool, error) {
-			role := cmn.NewAISRBACRole(ais)
-			return r.client.DeleteResourceIfExists(ctx, role)
-		},
-		func() (bool, error) {
-			sa := cmn.NewAISServiceAccount(ais)
-			return r.client.DeleteResourceIfExists(ctx, sa)
-		},
-	)
 }
 
 func hasFinalizer(ais *aisv1.AIStore) bool {
