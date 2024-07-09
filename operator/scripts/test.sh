1eac95ed6bcdcee7dba403a3ac94764f6b3f1602
Janusz Marcinkiewicz <januszm@nvidia.com>
2024-07-09 19:58:29 +0200

    operator: update layout version of project
    Signed-off-by: Janusz Marcinkiewicz <januszm@nvidia.com>

--- operator/scripts/test.sh
+++ operator/scripts/test.sh
@@ -3,16 +3,7 @@
 # NOTE: Currently, we only have integration tests that run on an existing K8s cluster.
 # `USE_EXISTING_CLUSTER=true` is set while running the tests to ensure, `envtest` environment isn't used.
 
-envtest_assets_dir="/tmp/ais-k8s-operator/testbin"
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-mkdir -p ${envtest_assets_dir}
-test -f ${envtest_assets_dir}/setup-envtest.sh || curl -sSLo ${envtest_assets_dir}/setup-envtest.sh https://raw.githubusercontent.com/kubernetes-sigs/controller-runtime/v0.7.0/hack/setup-envtest.sh
-
-source ${envtest_assets_dir}/setup-envtest.sh
-
-fetch_envtest_tools ${envtest_assets_dir}
-setup_envtest_env ${envtest_assets_dir}
 
 short=""
 long=""
