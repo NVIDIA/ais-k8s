c4c0f27259252e3f54035296409be2810c5e4322
Ryan Koo <rbk65@cornell.edu>
2025-07-11 10:28:10 -0700

    playbooks: bump operator version (undeploy operator)
    Signed-off-by: Ryan Koo <rbk65@cornell.edu>

--- playbooks/ais-deployment/roles/ais_undeploy_operator/files/undeploy-operator.sh
+++ playbooks/ais-deployment/roles/ais_undeploy_operator/files/undeploy-operator.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #
-# Deploy AIS operator
+# Undeploy AIS operator
 #
 
-release_version=${RELEASE:-v1.1.0}
+release_version=${RELEASE:-v2.4.0}
 
 kubectl delete -f https://github.com/NVIDIA/ais-k8s/releases/download/${release_version}/ais-operator.yaml
