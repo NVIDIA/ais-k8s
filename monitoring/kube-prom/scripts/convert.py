2a77a553402892ae7de2a73b512a0b316da4a97d
Aaron Wilson <aawilson@nvidia.com>
2025-08-29 17:49:28 -0500

    monitoring: Update alert conversion script to match new format with region list
    Signed-off-by: Aaron Wilson <aawilson@nvidia.com>

--- monitoring/kube-prom/scripts/convert.py
+++ monitoring/kube-prom/scripts/convert.py
@@ -21,46 +21,38 @@ def write_yaml(file, content):
     with open(file, "w") as out:
         yaml.dump(content, out, width=float("inf"))
 
-def process_rules(rules, output_dir, datasource):
+def process_rules(rules, output_dir, datasources):
     for rule in rules:
         output_path = Path(output_dir)
         output_path.mkdir(parents=True, exist_ok=True)
         alert_name = rule.get("alert")
         output_file = output_path.joinpath(f"{alert_name}.yaml")
-        rule["data"] = [{"datasourceUid": datasource}]
+        rule["regions"] = datasources
         labels = rule.get("labels", {})
         labels["team"] = "aistore"
         rule["labels"] = labels
         write_yaml(output_file, rule)
 
 
-class ParseOutputs(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        result = {}
-        for value in values:
-            folder, datasource = value.split('=', 1)
-            result[folder] = datasource
-        setattr(namespace, self.dest, result)
-
-
 def main(args):
     rules = load_rules(args.input)
-    for output_dir, datasource in args.outputs.items():
-        process_rules(rules, output_dir, datasource)
+    process_rules(rules, args.output, args.datasources)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Input file')
     parser.add_argument('--input', type=str, default='../values/alert-rules.yaml', required=False, help='Path to the input alert rules yaml.')
     parser.add_argument(
-        '--outputs',
-        nargs='*',  # accept multiple folder=datasource pairs
-        action=ParseOutputs,
-        default={
-            'Storage_AIStore_EAST': 'mimir-us-east-1',
-            'Storage_AIStore_WEST': 'mimir-us-west-2'
-        },
-        metavar='FOLDER=DATASOURCE',
-        help='Map output folders to their data sources, e.g. AIS-West=mimir-west AIS-East=mimir-east'
+        '--datasources',
+        type=lambda s: s.split(','),
+        default=['mimir-us-east-1', 'mimir-us-west-2', 'mimir-eu-north-1'],
+        metavar='DATASOURCE1,DATASOURCE2',
+        help='Comma-separated list of datasource names, e.g. mimir-us-east-1,mimir-us-west-2'
+    )
+    parser.add_argument(
+        '--output',
+        type=str,
+        default='Storage_AIStore',
+        help='Output folder for alert rules'
     )
 
     input_args = parser.parse_args()
