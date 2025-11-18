694e559ae2fcd288564f764b3eda2ce7098cb681
Abhishek Gaikwad <gaikwadabhishek1997@gmail.com>
2025-11-17 16:56:50 -0800

    follow-up: update operator version in helm and playbooks
    Signed-off-by: Abhishek Gaikwad <gaikwadabhishek1997@gmail.com>

--- playbooks/ais-deployment/roles/ais_undeploy_operator/files/undeploy-operator.sh
+++ playbooks/ais-deployment/roles/ais_undeploy_operator/files/undeploy-operator.sh
@@ -4,6 +4,6 @@
 # Undeploy AIS operator
 #
 
-release_version=${RELEASE:-v2.4.0}
+release_version=${RELEASE:-v2.8.0}
 
 kubectl delete -f https://github.com/NVIDIA/ais-k8s/releases/download/${release_version}/ais-operator.yaml
